61be1c7703d3bb5a9c7bc746547864831d45e999
AlanAlvarez <fantamigosmg@gmail.com>
2023-10-29 18:23:52 -0300

    primer commit

---
+++ src/tiposdedatos/Enteros.java
@@ -0,0 +1,56 @@
+package tiposdedatos;
+
+public class Enteros {
+	
+	public static void main(String[] args) {
+		
+		// Formas de declarar variables tipo entero
+		
+		int numero = 10;
+		
+		int numero2;
+		numero2 = 15;
+		
+		int numero3 = 2, numero5 = 3;
+		
+		System.out.println(numero2);
+		
+		// Tipo byte
+		
+		byte num1 = 20;
+		
+		
+		// Variable constante de tipo entero
+		final int variableConstante = 50;
+		
+		
+		short numeroShort = 122;
+		
+		long numeroLargo = 1234567899;
+		
+		
+		// Tipo double
+		
+		double numeroD = 12.9D;
+		
+		// Tipo flotante
+		
+		float numeroF = 10.20F;
+		
+		
+		// Tipo String
+		String palabra = "Hola Mundo";
+		
+		
+		// Tipo char
+		char caracter = 'a';
+		
+		
+		// Tipo Boleanos
+		boolean verdad = true;
+		boolean falsedad = false;
+		
+		
+	}
+
+}
